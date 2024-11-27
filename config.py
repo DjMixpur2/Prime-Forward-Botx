10f9888a10000f9e3c2134c42145d5fa614d99f9
DjMixpur2 <djmixpur2@gmail.com>
2024-11-27 20:38:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@ import datetime
 from os import environ 
 
 class Config:
-    API_ID = environ.get("API_ID", "")
-    API_HASH = environ.get("API_HASH", "")
-    BOT_TOKEN = environ.get("BOT_TOKEN", "") 
+    API_ID = environ.get("API_ID", "12850056")
+    API_HASH = environ.get("API_HASH", "15564ec4a1a2cbef87c99a9aa9e40b34")
+    BOT_TOKEN = environ.get("BOT_TOKEN", "8178732396:AAEd104z69Wv0xQel_fgdWONINLHbKRBRIg") 
     BOT_SESSION = environ.get("BOT_SESSION", "Prime-Forward-Botx") 
-    DATABASE_URI = environ.get("DATABASE", "mongodb+srv://becomi3205:YTY59pwNHUgCnpUf@cluster0.ily4e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    DATABASE_URI = environ.get("DATABASE", "mongodb+srv://Kdbhai:Kdbhai@cluster0.qxasr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
     DATABASE_NAME = environ.get("DATABASE_NAME", "Cluster0")
-    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '').split()]
+    BOT_OWNER_ID = [int(id) for id in environ.get("BOT_OWNER_ID", '770434685').split()]
     LOG_CHANNEL = int(environ.get('LOG_CHANNEL', ''))
     FORCE_SUB_CHANNEL = environ.get("FORCE_SUB_CHANNEL", "https://t.me/Prime_Botz") 
     FORCE_SUB_ON = environ.get("FORCE_SUB_ON", "True")
